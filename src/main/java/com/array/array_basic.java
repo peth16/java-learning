47ee280c488da6be4312f625e02aa71b03cc204b
peth16 <petharavind16@gmail.com>
2025-06-17 19:17:29 +0530

    Created new class for practicing the basic array.

---
+++ src/main/java/com/array/array_basic.java
@@ -0,0 +1,23 @@
+package com.array;
+
+public class array_basic {
+
+    public static void main(String[] args){
+
+        int[] array = new int[5];
+
+        array[0]=   20;
+        array[1]=   30;
+        array[2]=   40;
+        array[3]=   50;
+        array[4]=   60;
+
+        for (int i=0; i<array.length; i++){
+
+            System.out.println(array[i]);
+
+        }
+
+    }
+
+}
